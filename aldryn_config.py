b5df06e9e3836fd975636cb0478bd0150defe3ca
Joaquim Lozano <joaquimlozano@gmail.com>
2018-06-04 10:38:12 +0200

    all

--- aldryn_config.py
+++ aldryn_config.py
@@ -6,6 +6,11 @@ from aldryn_client import forms
 class Form(forms.BaseForm):
     google_ua = forms.CharField("Google UA", required=True)
 
+
     def to_settings(self, data, settings):
-        settings["GOOGLE_UA"] = data['google_ua']
+        settings['GOOGLE_UA'] = data.get('google_ua', '')
+        try:
+            settings['GOOGLE_UA'] = data.get('google_ua')
+        except (ValueError, TypeError):
+            pass
         return settings
