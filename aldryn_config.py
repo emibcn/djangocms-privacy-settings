141a0559a080e4c262601e65fa00caf8298ec10e
Joaquim Lozano <joaquimlozano@gmail.com>
2018-06-04 10:20:01 +0200

    Fix installed_apps

--- aldryn_config.py
+++ aldryn_config.py
@@ -7,7 +7,5 @@ class Form(forms.BaseForm):
     google_ua = forms.CharField("Google UA", required=True)
 
     def to_settings(self, data, settings):
-        settings["INSTALLED_APPS"].extend(["djangocms_privacy_settings"])
-
         settings["GOOGLE_UA"] = data['google_ua']
         return settings
--- djangocms_privacy_settings/__init__.py
+++ djangocms_privacy_settings/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 
 
 default_app_config = 'djangocms_privacy_settings.apps.DjangocmsPrivacySettingsConfig'
